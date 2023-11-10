de2b826921cb49f3afcc45a74dc58386b9bb4927
remysys <109655176+remysys@users.noreply.github.com>
2023-11-10 20:07:29 +0800

    feat(llvm-riscv-backend): 16. printInst

--- llvm-project/llvm/lib/Target/RRISCV/TargetDesc/RRISCVInstPrinter.cpp
+++ llvm-project/llvm/lib/Target/RRISCV/TargetDesc/RRISCVInstPrinter.cpp
@@ -1,18 +1,51 @@
 #include "RRISCVInstPrinter.h"
 #include "llvm/MC/MCInst.h"
 
+#define DEBUG_TYPE "rriscv instr printer"
+#include <llvm/Support/Debug.h>
+
 using namespace llvm;
 
 // Include the auto-generated portion of the assembly writer.
 #define PRINT_ALIAS_INSTR
 #include "RRISCVGenAsmWriter.inc"
 
-void RRISCVInstPrinter::printInst(MCInst const *MI, uint64_t Address, StringRef Annot, MCSubtargetInfo const &STI, raw_ostream &O) {}
+void RRISCVInstPrinter::printInst(MCInst const *MI, uint64_t Address,
+                                  StringRef Annot, MCSubtargetInfo const &STI,
+                                  raw_ostream &O) {
+  printInstruction(MI, Address, O);
+}
 
-void RRISCVInstPrinter::printRegName(raw_ostream &OS, unsigned RegNo) const {}
+void RRISCVInstPrinter::printRegName(raw_ostream &O, unsigned RegNo) const {
+  O << getRegisterName(RegNo);
+}
 
-// void RRISCVInstPrinter::printOperand(const MCInst *MI, unsigned OpNo, const MCSubtargetInfo &STI, raw_ostream &O, const char *Modifier) {}
+// void RRISCVInstPrinter::printOperand(const MCInst *MI, unsigned OpNo, const
+// MCSubtargetInfo &STI, raw_ostream &O, const char *Modifier) {}
 
-void RRISCVInstPrinter::printOperand(const MCInst *MI, unsigned OpNo, raw_ostream &OS) {}
+void RRISCVInstPrinter::printOperand(const MCInst *MI, unsigned OpNo,
+                                     raw_ostream &OS) {
+  const MCOperand &Op = MI->getOperand(OpNo);
+  if (Op.isReg()) {
+    printRegName(OS, Op.getReg());
+    return;
+  }
 
-void RRISCVInstPrinter::printOperand(const MCInst *MI, uint64_t _Address, unsigned OpNum, raw_ostream &O) {}
+  if (Op.isImm()) {
+    OS << Op.getImm();
+    return;
+  }
+}
+
+void RRISCVInstPrinter::printOperand(const MCInst *MI, uint64_t _Address,
+                                     unsigned OpNum, raw_ostream &O) {
+  printOperand(MI, OpNum, O);
+}
+
+void RRISCVInstPrinter::printMemOperand(const MCInst *MI, int opNum,
+                                        raw_ostream &O) {
+  printOperand(MI, opNum + 1, O);
+  O << "(";
+  printOperand(MI, opNum, O);
+  O << ")";
+}
