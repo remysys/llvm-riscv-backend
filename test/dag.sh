4dff5d0995540bffdf5f86c7ff7356f836809c0d
remysys <109655176+remysys@users.noreply.github.com>
2025-05-18 00:03:33 +0800

    feat(llvm-riscv-bankend): 41. support for more argument registers in RISC-V calling convention

--- test/dag.sh
+++ test/dag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BIN="../llvm-project/build/bin"
-FNAME="test21"
+FNAME="test22"
 
 SRC=$FNAME.cpp
 BC=$FNAME.bc
---
+++ test/test22.cpp
@@ -0,0 +1,8 @@
+int bar(int a0, int a1, int a2, int a3, int a4, int a5, int a6, int a7, int b) {
+  return a0 + a1 + a2 + a3 + a4 + a5 + a6 + a7 + b;
+}
+
+int foo() {
+  int ret = bar(10, 11, 12, 13, 14, 15, 16, 17, 18);
+  return ret;
+}
